e4bfc704b7af7e1c4e950bd0d3533cfcb94f1a01
Prakash-sa <sainiprakash525@gmail.com>
2019-10-11 21:35:49 +0530

    Temperature sensor gy-521

---
+++ IOT/Temperature/gy-521.ino
@@ -0,0 +1,27 @@
+#include "temp.h"
+
+
+const uint8_t scl=5;
+const uint8_t sda=4;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(sda,scl);
+   //setup_mpu_6050_registers();
+MPU6050_Init();
+}
+
+void loop() {
+  /*
+  read_mpu_6050_data();
+  */
+  double Ax, Ay, Az, T, Gx, Gy, Gz;
+  
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+  
+  float t = ((float)T/340.00)+36.53;
+  Serial.print("Temp :");
+  Serial.println(t);
+
+  Serial.println();
+
+}
---
+++ IOT/Temperature/temp.h
@@ -0,0 +1,26 @@
+#ifndef TEMP
+#define TEMP
+
+#include "Wire.h"
+extern int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+const uint8_t MPU6050SlaveAddress = 0x68;
+
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data);
+
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress);
+
+void MPU6050_Init();
+
+#endif;
